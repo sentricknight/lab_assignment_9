3115808beed6baa433e67346999c0986865524e5
sentricknight <64561889+sentricknight@users.noreply.github.com>
2023-04-14 22:33:01 -0400

    Update lab9.c

--- lab9.c
+++ lab9.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 // RecordType
 struct RecordType
 {
-	int		id;
-	char	name;
-	int		order; 
+    int     id;
+    char    name;
+    int     order;
+    struct RecordType* pNext;   // pointer to next record in the same index
 };
-#define HASH_SIZE == 10
 
 // Fill out this structure
 struct HashType
 {
-	 struct RecordType *pRecord;
+    struct RecordType* pRecord;
 };
 
 // Compute the hash function
@@ -24,50 +25,51 @@ int hash(int x)
 // parses input file to an integer array
 int parseData(char* inputFileName, struct RecordType** ppData)
 {
-	FILE* inFile = fopen(inputFileName, "r");
-	int dataSz = 0;
-	int i, n;
-	char c;
-	struct RecordType *pRecord;
-	*ppData = NULL;
+    FILE* inFile = fopen(inputFileName, "r");
+    int dataSz = 0;
+    int i, n;
+    char c;
+    struct RecordType *pRecord;
+    *ppData = NULL;
 
-	if (inFile)
-	{
-		fscanf(inFile, "%d\n", &dataSz);
-		*ppData = (struct RecordType*) malloc(sizeof(struct RecordType) * dataSz);
-		// Implement parse data block
-		if (*ppData == NULL)
-		{
-			printf("Cannot allocate memory\n");
-			exit(-1);
-		}
-		for (i = 0; i < dataSz; ++i)
-		{
-			pRecord = *ppData + i;
-			fscanf(inFile, "%d ", &n);
-			pRecord->id = n;
-			fscanf(inFile, "%c ", &c);
-			pRecord->name = c;
-			fscanf(inFile, "%d ", &n);
-			pRecord->order = n;
-		}
+    if (inFile)
+    {
+        fscanf(inFile, "%d\n", &dataSz);
+        *ppData = (struct RecordType*) malloc(sizeof(struct RecordType) * dataSz);
 
-		fclose(inFile);
-	}
+        if (*ppData == NULL)
+        {
+            printf("Cannot allocate memory\n");
+            exit(-1);
+        }
 
-	return dataSz;
+        for (i = 0; i < dataSz; ++i)
+        {
+            pRecord = *ppData + i;
+            fscanf(inFile, "%d ", &n);
+            pRecord->id = n;
+            fscanf(inFile, "%c ", &c);
+            pRecord->name = c;
+            fscanf(inFile, "%d ", &n);
+            pRecord->order = n;
+        }
+
+        fclose(inFile);
+    }
+
+    return dataSz;
 }
 
 // prints the records
 void printRecords(struct RecordType pData[], int dataSz)
 {
-	int i;
-	printf("\nRecords:\n");
-	for (i = 0; i < dataSz; ++i)
-	{
-		printf("\t%d %c %d\n", pData[i].id, pData[i].name, pData[i].order);
-	}
-	printf("\n\n");
+    int i;
+    printf("\nRecords:\n");
+    for (i = 0; i < dataSz; ++i)
+    {
+        printf("\t%d %c %d\n", pData[i].id, pData[i].name, pData[i].order);
+    }
+    printf("\n\n");
 }
 
 // display records in the hash structure
@@ -84,12 +86,11 @@ void displayRecordsInHash(struct HashType *pHashArray, int hashSz)
         {
             printf("Index %d -> ", i);
 
-            for (j=0; j < hashSz; ++j)
+            struct RecordType* pCur = pHashArray[i].pRecord;
+            while (pCur != NULL)
             {
-                if (pHashArray[i+j].pRecord != NULL)
-                {
-                    printf("%d %c %d -> ", pHashArray[i+j].pRecord->id, pHashArray[i+j].pRecord->name, pHashArray[i+j].pRecord->order);
-                }
+                printf("%d %c %d -> ", pCur->id, pCur->name, pCur->order);
+                pCur = pCur->pNext;
             }
 
             printf("\n");
@@ -119,5 +120,63 @@ int main(void)
         pHashTable[i].pRecord = NULL;
     }
 
-    for (i=0; i < recordSz;
+    // insert records into the hash table
+    for (i = 0; i < recordSz; ++i)
+{
+int idx = hash(pRecords[i].id);
+struct RecordType* pNewRecord = (struct RecordType*) malloc(sizeof(struct RecordType));
+if (pNewRecord == NULL)
+{
+printf("Cannot allocate memory\n");
+exit(-1);
+}
+pNewRecord->id = pRecords[i].id;
+pNewRecord->name = pRecords[i].name;
+pNewRecord->order = pRecords[i].order;
+pNewRecord->pNext = NULL;
+
+    if (pHashTable[idx].pRecord == NULL)
+    {
+        pHashTable[idx].pRecord = pNewRecord;
+    }
+    else
+    {
+        struct RecordType* pCur = pHashTable[idx].pRecord;
+        struct RecordType* pPrev = NULL;
+        while (pCur != NULL && pCur->id < pNewRecord->id)
+        {
+            pPrev = pCur;
+            pCur = pCur->pNext;
+        }
+
+        if (pPrev == NULL)
+        {
+            pNewRecord->pNext = pCur;
+            pHashTable[idx].pRecord = pNewRecord;
+        }
+        else
+        {
+            pPrev->pNext = pNewRecord;
+            pNewRecord->pNext = pCur;
+        }
+    }
+}
+
+displayRecordsInHash(pHashTable, hashSz);
+
+// Free memory
+for (i=0; i < hashSz; ++i)
+{
+    struct RecordType *pCur = pHashTable[i].pRecord;
+    while (pCur != NULL)
+    {
+        struct RecordType *pNext = pCur->pNext;
+        free(pCur);
+        pCur = pNext;
+    }
+}
+free(pHashTable);
+free(pRecords);
+
+return 0;
 }
